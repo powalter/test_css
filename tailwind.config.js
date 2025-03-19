768b30dfcd15465d2b212d4b4f164139a2ee9fea
powalter <walter.work@outlook.com.br>
2025-03-19 17:51:36 -0300

    19-03-25

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1D6D47',
+        secondary: '#2ecc71',
+      },
+    },
+  },
+  plugins: [],
+}
